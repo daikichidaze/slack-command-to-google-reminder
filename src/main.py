3481d6b17b9f3222af1fae1d6cd95b8ea87deaf3
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-02-23 21:58:36 +0700

    merge fast api & google reminder wraper

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 from google_reminder_api_wrapper import ReminderApi
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"test": "t"}
+
+
+@app.post("/set-remainder")
+async def set_remainder(text: str, dt: str):
+    try:
+        api = ReminderApi()
+        new_reminder = api.create(text, dt)
+        print(dt)
+    except:
+        return {"result": "Google reminder setting error"}
+    return {"text": "ok"}
