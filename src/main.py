558680b4dfb3683084077b13ef8d8375df124aff
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-02-24 21:19:14 +0700

    add user auth

--- src/main.py
+++ src/main.py
@@ -1,20 +1,54 @@
+import os
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from google_reminder_api_wrapper import ReminderApi
-from fastapi import FastAPI
+
+import db
 
 app = FastAPI()
+manager = LoginManager(os.environ['SECRET'], token_url='/auth/token')
 
 
 @app.get("/")
-async def root():
-    return {"test": "t"}
+def index(user=Depends(manager)):
+    return {"message": "API for google reminder"}
 
 
 @app.post("/set-remainder")
 async def set_remainder(text: str, dt: str):
+    # set_env_val(auth)
     try:
         api = ReminderApi()
         new_reminder = api.create(text, dt)
-        print(dt)
     except:
-        return {"result": "Google reminder setting error"}
-    return {"text": "ok"}
+        raise {"message": "Reminder setting error"}
+    return {"message": "Reminder added"}
+
+# For user auth
+
+
+@manager.user_loader()
+def load_user(email: str):
+    user = db.db.get(email)
+    return user
+
+
+@app.post('/auth/token')
+def login(data: OAuth2PasswordRequestForm = Depends()):
+    email = data.username
+    password = data.password
+
+    # we are using the same function to retrieve the user
+    user = load_user(email)
+    if not user:
+        raise InvalidCredentialsException  # you can also use your own HTTPException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+
+    access_token = manager.create_access_token(
+        data=dict(sub=email)
+    )
+    return {'access_token': access_token, 'token_type': 'bearer'}
