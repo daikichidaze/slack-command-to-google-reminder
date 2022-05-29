6418c55f61ede2284068558c82cc482215037a59
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-05-29 14:41:27 +0700

    add reminder registration from ifttt webhooks

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
 import hashlib
 import hmac
+import json
 import logging
 import threading
+from time import time
 import requests
+from datetime import datetime, timedelta
 from flask import Flask, request
 
 from google_reminder_api_wrapper import ReminderApi
@@ -11,6 +14,7 @@ from google_reminder_api_wrapper import ReminderApi
 SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
 SLACK_OAUTH_ACCESS_TOKEN = os.environ['SLACK_OAUTH_ACCESS_TOKEN']
 datetime_index = 16
+weight_reminder_title = 'Measure weight'
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -78,6 +82,38 @@ def register_from_slash():
 
     else:
         return ('Request failed verification.', 401)
+
+
+def convert_body_data(body: bytes) -> datetime:
+    str_date = body.decode('utf-8')
+    str_date = str_date.replace('\n', '')
+    body_dict = json.loads(str_date)
+
+    date_string = body_dict.get('datetime')
+    datetime_object = datetime.strptime(date_string, "%b %d, %Y at %I:%M%p")
+    return datetime_object
+
+
+def weight_reminder(body):
+    dt = convert_body_data(body)
+    tomorrow_noon = datetime(dt.year, dt.month, dt.day,
+                             11, 50) + timedelta(days=1)
+    str_tomorrow_noon = tomorrow_noon.strftime("%Y-%m-%d %H:%M")
+
+    # set google reminder
+    set_reminder(weight_reminder_title, str_tomorrow_noon)
+
+    return
+
+
+@app.route('/weight', methods=['POST'])
+def register_weight_reminder():
+    x = threading.Thread(
+        target=weight_reminder,
+        args=(request.data,)
+    )
+    x.start()
+    return ('Start register', 200)
 
 
 if __name__ == '__main__':
